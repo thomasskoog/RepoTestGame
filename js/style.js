9c36334c9b8c59bf601a6e9244ba88cc81dda324
Thomas Skoog <thomas.skoog@gmail.com>
2015-05-28 11:04:41 +0200

    påbörjad funktionalitet, variabler och slump-proto

---
+++ js/style.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Thomas on 28/05/2015.
+ */
+var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+var memory_values = [];
+var memory_platte_ids = [];
+var plattor_flippade = 0;
+
+Array.prototype.memory_platte_slump = function(){
+    var i = this.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
